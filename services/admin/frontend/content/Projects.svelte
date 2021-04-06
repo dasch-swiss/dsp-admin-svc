2586facbc1194cddc1bf53116d76d8290a3eace9
mdelez <60604010+mdelez@users.noreply.github.com>
2021-04-06 13:53:55 +0200

    style(project): adds fancy css

--- services/admin/frontend/content/Projects.svelte
+++ services/admin/frontend/content/Projects.svelte
@@ -21,25 +21,58 @@
   <p>Projects</p>
 
   {#if projectsList.projects}
+  <ul class="projects-list">
     {#each projectsList.projects as project}
-    <ul>
-      <li on:click="{() => handleProjectClick(project.id)}">
+      <button class="project" on:click="{() => handleProjectClick(project.id)}">
         {project.shortName}
-      </li>
-    </ul>
+      </button>
     {/each}
+  </ul>
   {:else}
-  <p>loading...</p>
+  <p>No Projects Found</p>
   {/if}
 
-  {#if currentProject}
+  {#if currentProject && projectsList.projects}
   <div>
     <div>ID: {currentProject.id}</div>
     <div>Title: {currentProject.shortName}</div>
-    <div>Description:</div>
-    <p>{currentProject.description}</p>
+    <div>Description: {currentProject.description}</div>
   </div>
   {:else}
   <p>Select a project</p>
   {/if}
 </main>
+
+<style>
+
+  main {
+    width: 400px;
+  }
+
+  .projects-list {
+    margin: 0px;
+  }
+
+  .project {
+    border: none;
+    display: block;
+    width: 100%;
+    font-size: 20px;
+  }
+
+  .project:nth-child(odd) {
+    background-color: rgba(192, 192, 192, 0.803);
+  }
+
+  .project:nth-child(odd):hover {
+    background-color: silver;
+  }
+
+  .project:nth-child(even) {
+    background-color: powderblue;
+  }
+
+  .project:nth-child(even):hover {
+    background-color: rgb(105, 217, 232);
+  }
+</style>
